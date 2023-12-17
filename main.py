fcb5bcc79e610046035bd79d800359bf064a1e6a
Adizasadov <adizbek.asadov@stud.uni-bamberg.de>
2023-12-17 11:36:01 +0100

    setup

---
+++ main.py
@@ -0,0 +1,11 @@
+print("Hello World")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<h1>Hello, World!</h1><a href='https://google.de'>Google</a>"
+
+if __name__ == '__main__':
+    app.run(debug=True)
