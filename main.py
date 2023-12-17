d1f234db1bbe32de7241fd0b63e24b957689c504
Adizasadov <adizbek.asadov@stud.uni-bamberg.de>
2023-12-17 12:59:59 +0100

    egal

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import torch
 
 print("Hello World")
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/example', methods=['POST'])
 def hello_world():
+    binary_data = request.data
+    with open('uploaded_file.bin', 'wb') as f:
+            f.write(binary_data)
     return "<h1>Hello, World!</h1><a href='https://google.de'>Google</a>"
 
 if __name__ == '__main__':
