125cfb7ecf08c4ce237043079b433d75b932d35c
diego411 <diego.enrico@directbox.com>
2024-01-20 22:33:39 +0100

    v0.0.3

--- main.py
+++ main.py
@@ -8,22 +8,22 @@ query_model.init()
 
 @app.route('/')
 def index():
-    return "Catmotion Server"
+    return "Catmotion Server v0.0.3"
 
 
 @app.route('/classify', methods=['POST'])
 def classify():
     binary_data = request.data
-    extension = '.wav'
+
+    extension = 'wav'
     date = time.ctime(time.time())
     file_path = f'audio/req_file_{date}.{extension}'
-
     with open(file_path, 'wb') as f:
         f.write(binary_data)
 
     predicted_class = query_model.classify(file_path)
     os.remove(file_path)
-    return predicted_class
+    return predicted_class, 200
 
 if __name__ == '__main__':
     app.run(debug=True)
--- query_model.py
+++ query_model.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torchaudio
 from torchvision import transforms
+import torchvision.models as models
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
@@ -10,44 +11,105 @@ from PIL import Image
 import time
 import os
 
+base_width = 224
+base_height = 224
+
+resnet18 = models.resnet18(pretrained=True)
+resnet18_features = nn.Sequential(*(list(resnet18.children())[:-1]))
+
+class ResCNNet(nn.Module):
+  def __init__(self, n_output=1, freeze=True):
+    super().__init__()
+    self.resnet = resnet18_features
+    if freeze:
+      self.freeze()
+    self.l1 = nn.Linear(512, n_output)
+
+  def forward(self, x):
+    x = self.resnet(x).squeeze()
+    return self.l1(x)
+
+  def freeze(self):
+    for param in self.resnet.parameters():
+      param.requires_grad = False
+
 class CNNet(nn.Module):
-    def __init__(self):
+    def __init__(self, n_output=1, conv_kernel_size=5, conv_stride=1, pool_kernel_size=2, pool_stride=2, l1=64, l2=50):
         super().__init__()
-        self.conv1 = nn.Conv2d(3, 32, kernel_size=5)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=5)
+        
+        self.conv_kernel_size = conv_kernel_size
+        self.conv_stride = conv_stride
+        self.pool_kernel_size = pool_kernel_size
+        self.pool_stride = pool_stride
+
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=self.conv_kernel_size, stride=self.conv_stride)
+
+        (width, height) = self.update_shape(base_width, base_height)
+        (width, height) = self.update_shape(width, height, is_pool=True)
+
+        self.conv2 = nn.Conv2d(32, l1, kernel_size=self.conv_kernel_size, stride=self.conv_stride)
+
+        (width, height) = self.update_shape(width, height)
+        (width, height) = self.update_shape(width, height, is_pool=True)
+
         self.conv2_drop = nn.Dropout2d()
         self.flatten = nn.Flatten()
-        self.fc1 = nn.Linear(51136, 50)
-        self.fc2 = nn.Linear(50, 4)
+        self.fc1 = nn.Linear(width * height * l1, l2)
+        self.fc2 = nn.Linear(l2, n_output)
 
 
     def forward(self, x):
-        x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        #x = x.view(x.size(0), -1)
+        x = self.conv1(x)
+        x = F.max_pool2d(x, kernel_size=self.pool_kernel_size, stride=self.pool_stride)
+        x = F.relu(x)
+        x = self.conv2(x)
+        x = self.conv2_drop(x)
+        x = F.max_pool2d(x, kernel_size=self.pool_kernel_size, stride=self.pool_stride)
+        x = F.relu(x)
         x = self.flatten(x)
-        x = F.relu(self.fc1(x))
+        x = self.fc1(x)
+        x = F.relu(x)
         x = F.dropout(x, training=self.training)
-        x = F.relu(self.fc2(x))
+        x = self.fc2(x)
+        x = F.relu(x)
+        
         return F.log_softmax(x,dim=1)
 
-model1 = None
-model2 = None
+    def update_shape(self, width, height, is_pool=False):
+        kernel_size = self.pool_kernel_size if is_pool else self.conv_kernel_size
+        stride = self.pool_stride if is_pool else self.conv_stride
+
+        width = int(((width - kernel_size) / stride) + 1)
+        height = int(((height - kernel_size) / stride) + 1)
+
+        return (width, height)
+
+beti = None
+kisha = None
 
 def init():
-    global model1, model2 
-    
-    model1 = CNNet()
-    model1.load_state_dict(torch.load('./models/model1_h15.pth'))
-    model1.eval()
-    
-    print("Initialized model 1")
-    
-    model2 = CNNet()
-    model2.load_state_dict(torch.load('./models/model2_h15.pth'))
-    model2.eval()
+    global beti, kisha 
 
-    print("Initialized model 2")
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    torch_device = torch.device(device)
+    
+    beti = CNNet(n_output = 4).to(device)
+    beti.load_state_dict(torch.load('./models/beti.pth', map_location=torch_device))
+    beti.eval()
+    
+    if beti is None:
+        print("Failed to initialize beti model")
+    else:
+        print("Initialized beti model")
+    
+    kisha = ResCNNet(n_output = 3, freeze = False).to(device)
+    kisha.load_state_dict(torch.load('./models/kisha.pth', map_location=torch_device))
+    kisha.eval()
+
+    if kisha is None:
+        print("Failed to initialize kisha model")
+    else: 
+        print("Initialized kisha model")
 
 def create_spectrogram(audio_path):
     waveform, sample_rate = torchaudio.load(audio_path)
@@ -71,7 +133,7 @@ def get_model_prediction(model, image_path):
 
     # Define the transformation to be applied to the input image
     transform=transforms.Compose([
-        transforms.Resize((201,81)),
+        transforms.Resize((base_width, base_height)),
         transforms.ToTensor()
     ])
 
@@ -86,8 +148,7 @@ def get_model_prediction(model, image_path):
         output = model(input_batch)
 
     # The 'output' variable now contains the model predictions for the given input image
-    probabilities = torch.exp(output)
-
+    probabilities = torch.exp(output) # maybe not needed
     # Find the index of the class with the highest probability (argmax)
     return torch.argmax(probabilities).item()
 
@@ -102,24 +163,18 @@ class_map1 = {
 class_map2 = {
     0: 'angry', 
     1: 'happy', 
-    2: 'sad', 
-    3: 'scared'
+    2: 'sad'
 }
 
 def classify(audio_path):
     image_path = create_spectrogram(audio_path)
     
-    predicted_class_index_m1 = get_model_prediction(model1, image_path)
+    predicted_class_beti = get_model_prediction(beti, image_path)
     
-    if predicted_class_index_m1 == 2: # Model 1 predicted meow
-        predicted_class_index_m2 = get_model_prediction(model2, image_path)
+    if predicted_class_beti == 2: # Model 1 predicted meow
+        predicted_class_kisha = get_model_prediction(kisha, image_path)
         os.remove(image_path)
-        return f'{class_map2[predicted_class_index_m2]} (Meow)'
+        return f'{class_map2[predicted_class_kisha]} (Meow)'
 
     os.remove(image_path)
-    return class_map1[predicted_class_index_m1]
-
-    # Optionally, you can also get the probability of the predicted class
-    #predicted_class_probability = probabilities[predicted_class_index].item()
-
-    #print("Probability:", predicted_class_probability)
+    return class_map1[predicted_class_beti]
